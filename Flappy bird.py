a04d028e9cb6a838afb26913870e648024e7a95a
jamesangie <bucketofpaste@gmail.com>
2020-06-07 15:58:37 -0400

    Updated comments Fixed a few bugs

--- Flappy bird.py
+++ Flappy bird.py
@@ -1,6 +1,7 @@
 import neat
 import time
 from Base import *
+import pickle
 
 pygame.init()
 
@@ -24,10 +25,16 @@ def draw_window(window, birds, pipe, base):
 
 
 def main(genomes, config):
+
+    # the network
     nets = []
+    # the set of genomes
     ge = []
+    # the list of birds
     birds = []
 
+    # by accessing genomes in Neat module, we can create each bird for each genome, we can keep track of the fitness of
+    # each bird and we can then create the training progress
     for _, g in genomes:
         net = neat.nn.FeedForwardNetwork.create(g, config)
         nets += [net]
@@ -48,34 +55,33 @@ def main(genomes, config):
     score = 0
     while running:
 
-
         clock.tick(30)
         event = pygame.event.poll()
         if event.type == pygame.QUIT:
             running = 0
 
-        pipe_ind = 0
-        if len(birds) > 0:
-            if len(pipes) > 1 and birds[0].x > pipes[pipe_count].x + pipes[pipe_count].DOWN_PIPE.get_width():
-                pipe_ind = 1
-        else:
-            running = False
-            break
-
         pipes[pipe_count].move()
         base.move()
         draw_window(window, birds, pipes[pipe_count], base)
 
+        # end the generation if no birds left
+        if len(birds) == 0:
+            running = False
+            break
+
+        # moves for each bird each frame
         for i, bird in enumerate(birds):
             bird.move()
+            # reward when the bird move(live for one more frame)
             ge[i].fitness += 0.1
-            # activate the neural network using the output
+            # activate the neural network using the output of how far the bird is from the gap for next pipe
             output = nets[i].activate((bird.y, abs(bird.y - pipes[pipe_count].height),
-                                                   abs(bird.y - pipes[pipe_count].bottom)))
+                                       abs(bird.y - pipes[pipe_count].bottom)))
             if output[0] > 0.5:
                 bird.jump()
 
-            # show the lose state when the bird collides with any pipe or off the screen
+            # remove the bird, the genome from the network and give punishments when the bird collides with any pipe or
+            # off the screen
             if pipes[pipe_count].collision(bird) or bird.y > 700 or bird.y < 0:
                 ge[i].fitness -= 1
                 birds.pop(i)
@@ -86,6 +92,7 @@ def main(genomes, config):
         # between real time and the frame in the game
         if pipes[pipe_count].x // 5 * 5 == 200:
             score += 1
+            # reward the birds that pass a pipe
             for g in ge:
                 g.fitness += 5
             print(score)
@@ -93,13 +100,18 @@ def main(genomes, config):
         if pipes[pipe_count].x == -80:
             pipes += [Pipe(500)]
             pipe_count += 1
+
+        if score > 30:
+            running = 0
+            break
+
         # jump the bird when click (that is how we play BABE!)
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            bird.jump()
+        # if event.type == pygame.MOUSEBUTTONDOWN:
+        # bird.jump()
 
 
-# set up Neat with default values defined in config-feedforward.txt
 def run(configpath):
+    # set up Neat with default values defined in config-feedforward.txt
     config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
                                 neat.DefaultSpeciesSet, neat.DefaultStagnation,
                                 configpath)
@@ -110,10 +122,11 @@ def run(configpath):
     stats = neat.StatisticsReporter()
     p.add_reporter(stats)
 
-    winner = p.run(main,50)
+    winner = p.run(main, 50)
 
 
 if __name__ == "__main__":
+    """open up configuration for NEAT and run the training"""
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, "config-feedforward.txt")
     run(config_path)
